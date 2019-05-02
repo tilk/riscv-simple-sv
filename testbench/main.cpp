3694d3ab06da7aedc589737cee254ec155ddf6ae
Marek Materzok <tilk@tilk.eu>
2019-05-02 18:25:38 +0200

    Verbose output toggle in testbench.

--- testbench/main.cpp
+++ testbench/main.cpp
@@ -25,24 +25,27 @@ int main(int argc, const char **argv, const char **env)
 {
     Verilated::commandArgs(argc, argv);
 
+    bool verbose = false;
     const char *str;
     str = Verilated::commandArgsPlusMatch("text_file=");
-    if (str) text_file = str + 11;
+    if (str && str[0]) text_file = str + 11;
     str = Verilated::commandArgsPlusMatch("data_file=");
-    if (str) data_file = str + 11;
+    if (str && str[0]) data_file = str + 11;
+    str = Verilated::commandArgsPlusMatch("verbose");
+    if (str && str[0]) verbose = true;
 
     std::unique_ptr<Vriscv_core> top(new Vriscv_core);
 
     top->reset = 0;
 
     for (int time = 0; time < 100000; time++) {
-        if (time > 10)
+        if (time > 9)
             top->reset = 0;
         else if (time > 4)
             top->reset = 1;
         top->clock = time & 1;
         top->eval();
-        if (top->clock) {
+        if (verbose && top->clock && time > 8) {
             std::cout << std::hex << std::setfill('0')
                       << "pc=" << std::setw(8) << top->pc << " "
                       << "inst=" << std::setw(8) << top->inst << " "
@@ -50,7 +53,8 @@ int main(int argc, const char **argv, const char **env)
                       << "in=" << std::setw(8) << top->bus_data_fetched << " "
                       << (top->bus_read_enable ? "1" : "0") << " "
                       << "out=" << std::setw(8) << top->bus_write_data << " "
-                      << (top->bus_write_enable ? "1" : "0") << " " << std::endl;
+                      << (top->bus_write_enable ? "1" : "0") << " " 
+                      << "fmt=" << (int)top->bus_format << std::endl;
         }
         if (top->bus_write_enable && top->bus_address == 0xfffffff0) {
             if (top->bus_write_data) {
