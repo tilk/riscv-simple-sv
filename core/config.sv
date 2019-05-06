79472e99e3421713c456f76f897dedc91b1878dc
Marek Materzok <tilk@tilk.eu>
2019-05-06 15:35:16 +0200

    Moved memory config to config.sv.

--- core/config.sv
+++ core/config.sv
@@ -7,8 +7,8 @@
 `define RV_CONFIG
 
 // Select architecture
-`define UNICICLO
-// `define MULTICICLO
+`define SINGLE_CYCLE
+// `define MULTI_CYCLE
 // `define PIPELINE
 
 // Select ISA
@@ -20,6 +20,28 @@
 
 // Enable debugging signals
 `define DEBUG
+
+//////////////////////////////////////////
+//              Memory config           //
+//////////////////////////////////////////
+
+// Program counter initial value
+`define INITIAL_PC      32'h00400000
+
+// Instruction memory
+`define TEXT_BEGIN      `INITIAL_PC
+`define TEXT_BITS       16
+`define TEXT_WIDTH      2**`TEXT_BITS
+`define TEXT_END        `TEXT_BEGIN + `TEXT_WIDTH - 1
+
+// Data memory
+`define DATA_BEGIN      32'h8000_0000
+`define DATA_BITS       17
+`define DATA_WIDTH      2**`DATA_BITS
+`define DATA_END        `DATA_BEGIN + `DATA_WIDTH - 1
+
+// Stack start
+`define STACK_ADDRESS   `DATA_END - 3
 
 // `define TEXT_HEX  "test.rom.vh"
 // `define DATA_HEX  "test.ram.vh"
--- core/constants.sv
+++ core/constants.sv
@@ -135,27 +135,5 @@
 `define CTL_PC_PC_IMM   2'b01
 `define CTL_PC_RS1_IMM  2'b10
 
-//////////////////////////////////////////
-//              Addresses               //
-//////////////////////////////////////////
-
-// Program counter initial value
-`define INITIAL_PC      32'h00400000
-
-// Instruction memory
-`define TEXT_BEGIN      `INITIAL_PC
-`define TEXT_BITS       16
-`define TEXT_WIDTH      2**`TEXT_BITS
-`define TEXT_END        `TEXT_BEGIN + `TEXT_WIDTH - 1
-
-// Data memory
-`define DATA_BEGIN      32'h8000_0000
-`define DATA_BITS       17
-`define DATA_WIDTH      2**`DATA_BITS
-`define DATA_END        `DATA_BEGIN + `DATA_WIDTH - 1
-
-// Stack start
-`define STACK_ADDRESS   `DATA_END - 3
-
 `endif
 
